bd9a21577bce2e7c24329e35ea285ced390c88f6
Ahmadreza <amiraligoodboy892@gmail.com>
2025-03-10 17:26:02 +0330

    Added cors to backend api.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
 
 // routes
 import authRoutes from "./routes/auth.route";
@@ -11,6 +12,7 @@ import taskRoutes from "./routes/task.route";
 import tagRoutes from "./routes/tag.route";
 
 export const app = new Hono().basePath("/api");
+app.use("/api/*", cors());
 
 export const routes = app
   // auth routes
