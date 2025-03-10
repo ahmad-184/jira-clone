3665af2e64e1ecac2b4a2227c779c7c2698ab52a
Ahmadreza <amiraligoodboy892@gmail.com>
2025-03-10 16:26:32 +0330

    Changed github color to white only.

--- src/app/(main)/(auth)/sign-in/_components/github-sign-in-button.tsx
+++ src/app/(main)/(auth)/sign-in/_components/github-sign-in-button.tsx
@@ -21,7 +21,7 @@ export default function GithubSignInButton({ className }: Props) {
         variant={"secondary"}
         className={cn("w-full", className)}
       >
-        <GithubIcon className="w-4 h-4" />
+        <GithubIcon className="w-4 h-4" fill="white" />
         Github
       </LoaderButton>
     </Link>
